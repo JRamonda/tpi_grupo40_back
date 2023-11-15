e8412efad92712865d3295589e27dd354153eaab
NicolasUrza <88108043+NicolasUrza@users.noreply.github.com>
2023-11-15 00:01:25 -0300

    generacion automatica de la fecha hora de creacion de estacion

--- Estaciones/src/main/java/com/frc/utn/grupo40/Estaciones/Entities/Estacion.java
+++ Estaciones/src/main/java/com/frc/utn/grupo40/Estaciones/Entities/Estacion.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -22,8 +23,9 @@ public class Estacion {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int id;
     private String nombre;
-    @Column(name = "fecha_hora_creacion")
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    @Column(name = "fecha_hora_creacion", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreationTimestamp
     private LocalDateTime fechaHoraCreacion;
     private double latitud;
     private double longitud;
